fcbbf7961ebcc98fe49cdbe7db9783d2ab17af09
AndreyGOIT <ae.borga@gmail.com>
2023-01-17 14:55:36 +0200

    all items in row

--- src/components/FetchCountries/FetchCountries.jsx
+++ src/components/FetchCountries/FetchCountries.jsx
@@ -5,8 +5,6 @@ import styles from './FetchCountries.module.css';
 // import { Link } from 'react-router-dom';
 
 export default function FetchCountries() {
-  // const keyAPI = '1eb36deae800d0e3d9fd1b0466458d26';
-  // const url = 'https://api.themoviedb.org/3/movie/';
   const [countries, setCountries] = useState(null);
   //   const location = useLocation;
 
@@ -19,23 +17,40 @@ export default function FetchCountries() {
     <>
       {countries && (
         <>
-          <ul>
-            {countries.map(({ area, flag, region, population }) => (
-              <li key={area + population} className={styles.listItem}>
-                <p>{flag}</p>
-                <p>{region}</p>
-                <p>{population}</p>
-              </li>
-              // <li className={styles.listItem} key={index.toString()}>
-              //   <Link
-              //     to={`/movies/${id}`}
-              //     state={location.state}
-              //     className={styles.link}
-              //   >
-              //     {title}
-              //   </Link>
-              // </li>
-            ))}
+          <div className={styles.topRow}>
+            <p>Flag</p>
+            <p>Name</p>
+            <p>Region</p>
+            <p>Population</p>
+            <p>Languages</p>
+          </div>
+          <ul className={styles.list}>
+            {countries.map(
+              ({ area, name, region, population, flags, languages }) => (
+                <li key={area + population} className={styles.listItem}>
+                  <img
+                    src={flags.png}
+                    alt={'contry'}
+                    width={'80'}
+                    height={'80'}
+                  />
+                  <p>{name.common}</p>
+                  <p>{region}</p>
+                  <p>{population}</p>
+
+                  <p>languages</p>
+                </li>
+                // <li className={styles.listItem} key={index.toString()}>
+                //   <Link
+                //     to={`/movies/${id}`}
+                //     state={location.state}
+                //     className={styles.link}
+                //   >
+                //     {title}
+                //   </Link>
+                // </li>
+              )
+            )}
           </ul>
           {/* <Outlet /> */}
         </>
