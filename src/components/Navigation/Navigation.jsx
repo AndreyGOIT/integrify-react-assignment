2716cea8e6d6a513afa823e93cfddf5ed1419edb
AndreyGOIT <ae.borga@gmail.com>
2023-01-19 15:21:36 +0200

    Home link

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,4 +1,4 @@
-// import { Link } from './Navigation.styled';
+import { Link } from 'react-router-dom';
 import styles from './Navigation.module.css';
 
 export default function Navigation() {
@@ -6,7 +6,10 @@ export default function Navigation() {
     <>
       <nav>
         <div className={styles.bar}>
-          <p className={styles.barText}>Country</p>
+          <Link to="/" className={styles.homeLink}>
+            Home
+          </Link>
+          {/* <p className={styles.barText}>Country</p> */}
           <input type="text" placeholder="Search by country name" />
         </div>
       </nav>
